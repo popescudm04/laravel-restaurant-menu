e97e066c0c3825c3395c2602df1d7d650c997c29
Popescu Dragos <popescudm04@gmail.com>
2023-11-09 11:20:11 +0200

    Added sidebar and navbar layout for admin

---
+++ resources/views/admin/admin.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+
+</head>
+
+<body>
+
+    @include('layouts.navigation')
+    
+
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('/js/script.js') }}"></script>
+
+</body>
+
+</html>
---
+++ resources/views/menu.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <p>Menu</p>
+</div>
